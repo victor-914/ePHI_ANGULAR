678b1c4ba41bf9d1b963c6d020dc562f2991f145
victor <103121630+victor-914@users.noreply.github.com>
2024-02-26 16:38:11 +0100

    new fix

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Store, StoreFeatureModule } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription, observeOn } from 'rxjs';
 import { toggleMenu } from '../../../store/menu/menu.action';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import {
   NavigationStart,
   Router,
@@ -26,29 +27,49 @@ import {
 export class SideMenuComponent {
   isOpen$!: Observable<boolean>;
   isOpen: any;
+  isMobile: boolean = false;
+  storeSubscription: Subscription = new Subscription();
+  breakpointSubscription: Subscription = new Subscription();
 
   constructor(
     private store: Store<{ isOpen: boolean }>,
-    private router: Router
-  ) {
-    this.store.select('isOpen').subscribe((isOpen) => {
-      // console.log('🚀 ~ NavBarComponent ~ constructor ~ isOpen:', this.isOpen);
+    private router: Router,
+    private breakpointObserver: BreakpointObserver
+  ) {}
+
+  ngOnInit(): void {
+    this.storeSubscription = this.store.select('isOpen').subscribe((isOpen) => {
       this.isOpen = isOpen;
       return (this.isOpen = this.isOpen?.isOpen);
     });
 
-    this.router.events.subscribe(event => {
+    this.router.events.subscribe((event) => {
       if (event instanceof NavigationStart) {
-        this.store.dispatch(toggleMenu());
+        this.breakpointSubscription = this.breakpointObserver
+          .observe([Breakpoints.Handset])
+          .subscribe((result: any) => {
+            if (
+              result.breakpoints[
+                '(max-width: 599.98px) and (orientation: portrait)'
+              ]
+            ) {
+              this.store.dispatch(toggleMenu());
+            }
+          });
       }
     });
   }
 
-  ngOnInit(): void {}
-
   toggleMenu() {
     this.store.dispatch(toggleMenu());
-    // console.log("🚀 ~ NavBarComponent ~ isOpen$:",this.isOpen$)
-    // console.log('Menu is open toggle:', this.isOpen);
+  }
+
+  ngOnDestroy(): void {
+    if (this.storeSubscription) {
+      this.storeSubscription.unsubscribe();
+    }
+    if (this.breakpointSubscription) {
+      this.breakpointSubscription.unsubscribe();
+    }
   }
 }
